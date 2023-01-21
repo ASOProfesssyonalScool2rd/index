77168808abd6015c888a32b178cad1ed4181f0fb
Uchidakouei <kouei28poiou@icloud.com>
2023-01-21 23:25:34 +0900

    Cheanged 2023/01/21

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-function OnButtonClick(){
+//1個目のJavaScript
+
+function OnButtonClick()
+{
     target = document.getElementById("output");
     target.innerHTML = "Helo World!!";
-    window.alert("プログラム界隈の定番の文字が表示されます");
+    window.alert("Q1プログラム界隈の定番の文字が表示されます");
+}
+//2個目のJavaScript
+
+function main()
+{
+   target = document.getElementById("main");
+   target.innerHTML = "これってなんだろう？";
+   window.alert("なんだっけ？")
+}
+//3個目のJavaScript
+
+function button()
+{
+    target = document.getElementById("button");
+    target.innerHTML = "最後のボタン動作です";
+    window.alert("最後の文字列はなんだろう？ｗ");
 }
